6c3cbd20fde90a0a65c572a5218ac59740d1c10a
Thenthprime <122050458+Thenthprime@users.noreply.github.com>
2024-10-06 15:50:19 -0500

    Add swipe functionality

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,6 +35,7 @@ android {
     }
 }
 
+
 dependencies {
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation ("androidx.constraintlayout:constraintlayout:2.1.3")
--- app/src/main/java/com/example/sem/ShowAllEvents.java
+++ app/src/main/java/com/example/sem/ShowAllEvents.java
@@ -1,11 +1,16 @@
 package com.example.sem;
 
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -14,6 +19,7 @@ import java.util.ArrayList;
 public class ShowAllEvents extends AppCompatActivity implements recyclerAdapter.RecyclerViewClickListener {
     private ArrayList<Event> eventsList;
     private RecyclerView recyclerView;
+    private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,8 +30,41 @@ public class ShowAllEvents extends AppCompatActivity implements recyclerAdapter.
 
         eventsList = loadEventData();
         setAdapter();
+
+        //implement swipe left/right
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(callBackMethod);
+        itemTouchHelper.attachToRecyclerView(recyclerView);
     }
-    @Override
+
+    ItemTouchHelper.SimpleCallback callBackMethod = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.RIGHT | ItemTouchHelper.LEFT){
+        @Override
+        public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+            return false;
+        }
+
+        @Override
+        public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+            int position = viewHolder.getAdapterPosition();
+
+            switch(direction){
+                case ItemTouchHelper.LEFT:
+                    //do left action
+                    eventsList.remove(position);
+                    recyclerView.getAdapter().notifyItemRemoved(position);
+                    Toast toastLeft = Toast.makeText(recyclerView.getContext(), "item removed", Toast.LENGTH_SHORT);
+                    toastLeft.show();
+                    break;
+                case ItemTouchHelper.RIGHT:
+                    //do right action
+                    eventsList.remove(position);
+                    recyclerView.getAdapter().notifyItemRemoved(position);
+                    Toast toastRight = Toast.makeText(recyclerView.getContext(), "item removed", Toast.LENGTH_SHORT);
+                    toastRight.show();
+                    break;
+            }
+        }
+
+    };
     public void recyclerViewListClicked(View v, int position) {
         Event selectedEvent = eventsList.get(position);
         Intent intent = new Intent(ShowAllEvents.this, EventOnClick.class);
