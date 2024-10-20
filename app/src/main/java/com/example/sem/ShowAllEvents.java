a0c95948c91722045eb2c40804a9873b101d7117
Daniel <danielkumankumah@gmail.com>
2024-10-20 00:07:46 -0400

    it is nailed

--- app/src/main/java/com/example/sem/ShowAllEvents.java
+++ app/src/main/java/com/example/sem/ShowAllEvents.java
@@ -26,6 +26,7 @@ import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class ShowAllEvents extends AppCompatActivity implements recyclerAdapter.RecyclerViewClickListener {
     private ArrayList<Event> eventsList;
@@ -40,8 +41,6 @@ public class ShowAllEvents extends AppCompatActivity implements recyclerAdapter.
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.all_events);
-
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         recyclerView = findViewById(R.id.recycler_view_events);
         allEventsList = new ArrayList<>();
